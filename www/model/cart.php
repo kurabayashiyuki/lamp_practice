db8f25843e07cc7ca71144fb0283c79611277980
zr7chapman <hiz.chapman@gmail.com>
2020-07-15 18:46:17 +0900

    Revert "追加画面の作成"

--- www/model/cart.php
+++ www/model/cart.php
@@ -1,9 +1,8 @@
-<?php
+<?php 
 require_once MODEL_PATH . 'functions.php';
 require_once MODEL_PATH . 'db.php';
 
-function get_user_carts($db, $user_id)
-{
+function get_user_carts($db, $user_id){
   $sql = "
     SELECT
       items.item_id,
@@ -22,13 +21,12 @@ function get_user_carts($db, $user_id)
     ON
       carts.item_id = items.item_id
     WHERE
-      carts.user_id = ?
+      carts.user_id = {$user_id}
   ";
-  return fetch_all_query($db, $sql, [$user_id]);
+  return fetch_all_query($db, $sql);
 }
 
-function get_user_cart($db, $user_id, $item_id)
-{
+function get_user_cart($db, $user_id, $item_id){
   $sql = "
     SELECT
       items.item_id,
@@ -47,205 +45,115 @@ function get_user_cart($db, $user_id, $item_id)
     ON
       carts.item_id = items.item_id
     WHERE
-      carts.user_id = ?
+      carts.user_id = {$user_id}
     AND
-      items.item_id = ?
+      items.item_id = {$item_id}
   ";
-  return fetch_query($db, $sql, [$user_id, $item_id]);
+
+  return fetch_query($db, $sql);
+
 }
 
-function add_cart($db, $user_id, $item_id)
-{
+function add_cart($db, $user_id, $item_id ) {
   $cart = get_user_cart($db, $user_id, $item_id);
-  if ($cart === false) {
+  if($cart === false){
     return insert_cart($db, $user_id, $item_id);
   }
   return update_cart_amount($db, $cart['cart_id'], $cart['amount'] + 1);
 }
 
-function insert_cart($db, $user_id, $item_id, $amount = 1)
-{
+function insert_cart($db, $user_id, $item_id, $amount = 1){
   $sql = "
-      INSERT INTO
-        carts(
-          item_id,
-          user_id,
-          amount
-        )
-      VALUES(?, ?, ?)
-    ";
-  return execute_query($db, $sql, [$item_id, $user_id, $amount]);
+    INSERT INTO
+      carts(
+        item_id,
+        user_id,
+        amount
+      )
+    VALUES({$item_id}, {$user_id}, {$amount})
+  ";
+
+  return execute_query($db, $sql);
 }
 
-function update_cart_amount($db, $cart_id, $amount)
-{
+function update_cart_amount($db, $cart_id, $amount){
   $sql = "
-      UPDATE
-        carts
-      SET
-        amount = ?
-      WHERE
-        cart_id = ?
-      LIMIT 1
-    ";
-  return execute_query($db, $sql, [$amount, $cart_id]);
+    UPDATE
+      carts
+    SET
+      amount = {$amount}
+    WHERE
+      cart_id = {$cart_id}
+    LIMIT 1
+  ";
+  return execute_query($db, $sql);
 }
 
-function delete_cart($db, $cart_id)
-{
+function delete_cart($db, $cart_id){
   $sql = "
-      DELETE FROM
-        carts
-      WHERE
-        cart_id = ?
-      LIMIT 1
-    ";
-  return execute_query($db, $sql, [$cart_id]);
+    DELETE FROM
+      carts
+    WHERE
+      cart_id = {$cart_id}
+    LIMIT 1
+  ";
+
+  return execute_query($db, $sql);
 }
 
-function purchase_carts($db, $carts)
-{
-  if (validate_cart_purchase($carts) === false) {
+function purchase_carts($db, $carts){
+  if(validate_cart_purchase($carts) === false){
     return false;
   }
-
-  $db->beginTransaction();
-
-  foreach ($carts as $cart) {
-    if (update_item_stock(
-      $db,
-      $cart['item_id'],
-      $cart['stock'] - $cart['amount']
-    ) === false) {
+  foreach($carts as $cart){
+    if(update_item_stock(
+        $db, 
+        $cart['item_id'], 
+        $cart['stock'] - $cart['amount']
+      ) === false){
       set_error($cart['name'] . 'の購入に失敗しました。');
     }
   }
-
+  
   delete_user_carts($db, $carts[0]['user_id']);
-
-  add_purchase_data($db, $carts);
-  if (has_error()) {
-    $db->rollback();
-  } else {
-    $db->commit();
-  }
 }
 
-
-function delete_user_carts($db, $user_id)
-{
+function delete_user_carts($db, $user_id){
   $sql = "
-      DELETE FROM
-        carts
-      WHERE
-        user_id = ?
-    ";
-  return execute_query($db, $sql, [$user_id]);
+    DELETE FROM
+      carts
+    WHERE
+      user_id = {$user_id}
+  ";
+
+  execute_query($db, $sql);
 }
 
 
-function sum_carts($carts)
-{
+function sum_carts($carts){
   $total_price = 0;
-  foreach ($carts as $cart) {
+  foreach($carts as $cart){
     $total_price += $cart['price'] * $cart['amount'];
   }
   return $total_price;
 }
 
-function validate_cart_purchase($carts)
-{
-  if (count($carts) === 0) {
+function validate_cart_purchase($carts){
+  if(count($carts) === 0){
     set_error('カートに商品が入っていません。');
     return false;
   }
-  foreach ($carts as $cart) {
-    if (is_open($cart) === false) {
+  foreach($carts as $cart){
+    if(is_open($cart) === false){
       set_error($cart['name'] . 'は現在購入できません。');
     }
-    if ($cart['stock'] - $cart['amount'] < 0) {
+    if($cart['stock'] - $cart['amount'] < 0){
       set_error($cart['name'] . 'は在庫が足りません。購入可能数:' . $cart['stock']);
     }
   }
-  if (has_error() === true) {
+  if(has_error() === true){
     return false;
   }
   return true;
 }
 
-
-function add_purchase_data($db, $carts)
-{
-  if (add_history($db, $carts[0]['user_id'], sum_carts($carts)) === false) {
-    return false;
-  }
-  $order_id = $db->lastInsertId('order_id');
-  foreach ($carts as $cart) {
-    if (add_statement($db, $order_id, $cart['item_id'], $cart['price'], $cart['amount']) === false) {
-      return false;
-    }
-  }
-}
-
-function add_history($db, $user_id, $total)
-{
-  $sql = "
-      INSERT INTO purchase_history(user_id, sum_purchase)
-      VALUES (?, ?)
-    ";
-  return execute_query($db, $sql, [$user_id, $total]);
-}
-
-function add_statement($db, $order_id, $item_id, $price, $amount)
-{
-  $sql = "
-      INSERT INTO purchase_statement(order_id, item_id, price, amount)
-      VALUES (?, ?, ?, ?)
-      ";
-  return execute_query($db, $sql, [$order_id, $item_id, $price, $amount]);
-}
-
-function purchase_history($db, $user_id = null)
-{
-  $var = [];
-  $sql =  "
-        SELECT 
-          purchase_history.order_id,
-          purchase_history.sum_purchase,
-          purchase_history.created
-        FROM
-          purchase_history";
-  if ($user_id !== null) {
-    $sql .=  " WHERE
-          purchase_history.user_id = ?";
-    $var[] = $user_id;
-  }
-  return fetch_all_query($db, $sql, $var);
-}
-
-function purchase_statement($db, $order_id, $user_id = null)
-{
-  $var = [$order_id];
-  $sql =  "
-        SELECT
-          items.name,
-          purchase_statement.price,
-          purchase_statement.amount,
-          purchase_statement.price * amount as subtotal
-        FROM
-          purchase_statement
-        JOIN
-          items
-        ON
-          purchase_statement.item_id = items.item_id
-        WHERE
-          purchase_statement.order_id = ?
-        ";
-  if ($user_id !== null) {
-    $sql .= "AND exists(SELECT * FROM purchase_history WHERE order_id = purchase_statement.order_id AND user_id = ?)";
-    $var[] = $user_id;
-  }
-  // var_dump($sql,$order_id,$user_id);
-  return fetch_all_query($db, $sql, $var);
-}
-// 小計の出し方がわからない
